c33b5a85afbae2ba184cf717e57b3ff14556c08b
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2026-05-18 10:12:07 +0530

    refactor: remove ping-pong tool and related imports from HelpfulAssistantAgent

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,12 +1,9 @@
 """Tools package."""
 
-from .ping_pong import make_prefixed_ping_pong_tool, ping_pong
 from .scrape import make_scrape_tool
 from .search import make_search_tool
 
 __all__ = [
-    "ping_pong",
-    "make_prefixed_ping_pong_tool",
     "make_scrape_tool",
     "make_search_tool",
 ]
