53cf1711690b5d4cdbe71c97ff296277cceae0ab
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2026-05-18 14:55:32 +0530

    chore: remove unused HelpfulAssistantAgent and update exports

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,11 +1,9 @@
 """Agents package."""
 
 from .base import BaseAgent
-from .helpful_assistant import HelpfulAssistantAgent
 from .playquery import PlayQueryAgent
 
 __all__ = [
     "BaseAgent",
-    "HelpfulAssistantAgent",
     "PlayQueryAgent",
 ]
